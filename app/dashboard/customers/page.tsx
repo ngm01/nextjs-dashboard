56978e958ca5a8f83eb17aa54642f4ba27842dce
ngm01 <nathaniel.g.moore@gmail.com>
2023-11-15 23:54:39 -0500

    chapter 5

--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -1,3 +1,6 @@
 export default function Page() {
-    return <p>Welcome to the Customers page</p>
+    return <div>
+            <a href="/dashboard/invoices">Go to Invoices</a>
+            <p>Welcome to the Customers page</p>
+        </div>
 }
--- app/dashboard/invoices/page.tsx
+++ app/dashboard/invoices/page.tsx
@@ -1,3 +1,8 @@
 export default function Page() {
-    return <p>Invoices page</p>
+    return (
+        <div>
+            <a href="/dashboard/customers">Go to Customers</a>
+            <p>Welcome to Invoices!</p>
+        </div>
+    )
 }
