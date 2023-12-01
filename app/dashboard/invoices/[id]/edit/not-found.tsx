4426943098d4fcd2428d640f69a06b8b3bc5b8c0
ngm01 <nathaniel.g.moore@gmail.com>
2023-11-30 22:43:29 -0500

    nextjs tutorial chp 14

--- app/dashboard/invoices/[id]/edit/not-found.tsx
+++ app/dashboard/invoices/[id]/edit/not-found.tsx
@@ -6,7 +6,7 @@ export default function NotFound() {
         <main className="flex h-full flex-col items-center justify-center gap-2">
             <FaceFrownIcon className="w-10 text-gray-400" />
             <h2 className="text-xl font-semibold">404 Not Found</h2>
-            <p>Sorry, we couldn't find the requested invoice.</p>
+            <p>Sorry, we couldn&apos;t find the requested invoice.</p>
             <Link href="/dashboard/invoices"
             className="mt-4 px-4 py-2 rounded-md bg-blue-500 text-white transition-colors hover:bg-blue-400" >
                 Go Back
